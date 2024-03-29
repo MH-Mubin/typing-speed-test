0c60502adb4bc7c08b6cce8e4ec0f60191084b15
Mubin <mahmudhasanmubin1996@gmail.com>
2024-03-29 23:39:16 +0600

    added some basic structures

--- typing.js
+++ typing.js
@@ -1,5 +1,11 @@
 const words='Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, explicabo!'.split(' ');
-
+function randomWord(){
+    
+    return 'test';
+}
 function newGame(){
     document.getElementById('words').innerHTML='';
+    for(let i=0; i<200; i++){
+
+    }
 }
