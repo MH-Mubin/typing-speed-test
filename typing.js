1a9f77da01457f581447df3506e1b8cc3c9aff32
Mubin <mahmudhasanmubin1996@gmail.com>
2024-03-30 23:52:23 +0600

    added random word

--- typing.js
+++ typing.js
@@ -1,11 +1,19 @@
 const words='Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, explicabo!'.split(' ');
+const wordsCount = words.length;
+
 function randomWord(){
-    
-    return 'test';
+    const randomIndex = Math.ceil(Math.random() * wordsCount); //deleted x: according to video
+    return words[randomIndex];
 }
+function formatWord(word){
+    return `<div class="word">${word}</div>`;
+}
+
 function newGame(){
     document.getElementById('words').innerHTML='';
     for(let i=0; i<200; i++){
-
+        document.getElementById('words').innerHTML += formatWord(randomWord());
     }
 }
+
+newGame();
