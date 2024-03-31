eb68a8e32a0e20037a6b86499f84da2172dc541c
Mubin <mahmudhasanmubin1996@gmail.com>
2024-03-31 23:45:04 +0600

    added few changes

--- typing.js
+++ typing.js
@@ -6,7 +6,11 @@ function randomWord(){
     return words[randomIndex];
 }
 function formatWord(word){
-    return `<div class="word">${word}</div>`;
+    return `<div class="word">
+        <span class="letter">
+            ${word.split('').join('</span><span class="letter">')}
+        </span>
+    </div>`;
 }
 
 function newGame(){
