0a155d8bd2fb87097c7fd2197fab1014b0acead0
Mahmud Hasan Mubin <mahmudhasanmubin1996@gmail.com>
2024-04-22 23:30:25 +0600

    added a little bit JS

--- typing.js
+++ typing.js
@@ -47,6 +47,11 @@ document.getElementById('game').addEventListener('keyup', ev =>{
             if (currentLetter.nextSibling){
                 addClass(currentLetter.nextSibling, 'current');
             }
+        } else{
+            const incorrectLetter = document.createElement('span')
+            incorrectLetter.innerHTML = key;
+            incorrectLetter.className = 'letter incorrect extra';
+            currentWord.appendChild(incorrectLetter);
         }
     }
     if (isSpace) {
