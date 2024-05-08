a453493667d988cb84ce0371fed69c6d9e5b4930
Mahmud Hasan Mubin <mahmudhasanmubin1996@gmail.com>
2024-05-08 12:19:58 +0600

    almost finished

--- typing.js
+++ typing.js
@@ -3,6 +3,7 @@ const wordsCount = words.length;
 const gameTime = 30*1000;
 window.timer = null;
 window.gameStart = null;
+window.pauseTime = 0;
 
 function addClass(el,name){
     el.className += ' '+name;
